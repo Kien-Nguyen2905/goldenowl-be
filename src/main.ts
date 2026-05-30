cea9ed08bd6ba234d2c90d18a5b5adc0293b2db1
Daisy <kiennguyen12.com@gmail.com>
2026-05-31 00:10:11 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(8080);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 8080;
+
+  await app.listen(port);
+
+  console.log(`Application running on port ${port}`);
 }
+
 bootstrap();
